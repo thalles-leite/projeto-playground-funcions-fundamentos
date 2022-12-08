d29cc92e600d1affae3c89426f02ad0d61f92fe4
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-07 23:59:31 -0300

    Exclusão do arquivo teste.js

--- src/teste.js
+++
@@ -1,6 +0,0 @@
-// condição 1 - linea < lineB + lineC && linea > modulo (lineB - lineC)
-// ou
-// linea < lineB + linec && lineb < linea + linec && linec < lineb+linea
-
-let arr = ['a', 'b', 'c'];
-console.log(arr.at[-1]);
