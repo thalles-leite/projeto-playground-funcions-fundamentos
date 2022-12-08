e1cc80b89cf78533a023fe27541d04b18254e7c0
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-07 23:14:08 -0300

    retorno true para primeira condição.

--- src/teste.js
+++ src/teste.js
@@ -1,10 +1,19 @@
-const firstCondition = (lineA,lineB,lineC) => {
-  const lados = [lineA, lineB, lineC];
-}
+// condição 1 - linea < lineB + lineC && linea > modulo (lineB - lineC)
+// ou
+// linea < lineB + linec && lineb < linea + linec && linec < lineb+linea
+// eslint-disable-next-line sonarjs/cognitive-complexity, complexity
+const fCondition = (lineA, lineB, lineC) => {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) return true;
+  if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) return true;
+  if (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA)) return true;
+  return false;
+};
 
+const sCondition = (lineA, lineB, lineC) => {
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) return true;
+  return false;
+};
 
-const triangleCheck = (lineA, lineB, lineC) => {
-  // condição 1 - linea < lineB + lineC && linea > modulo (lineB - lineC)
-  // ou
-  // linea < lineB + linec && lineb < linea + linec && linec < lineb+linea
-  };
+const triangleCheck = (lA, lB, lC) => fCondition(lA, lB, lC) && sCondition(lA, lB, lC);
+
+console.log(triangleCheck(10, 14, 8));
