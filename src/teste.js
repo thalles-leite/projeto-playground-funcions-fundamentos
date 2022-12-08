c6afcfa2703da5643977a220045a1b7caa2387cf
Thalles Leite <eng.thallesleite@gmail.com>
2022-12-07 23:37:03 -0300

    Todos os testes passaram.

--- src/teste.js
+++ src/teste.js
@@ -1,19 +1,6 @@
 // condição 1 - linea < lineB + lineC && linea > modulo (lineB - lineC)
 // ou
 // linea < lineB + linec && lineb < linea + linec && linec < lineb+linea
-// eslint-disable-next-line sonarjs/cognitive-complexity, complexity
-const fCondition = (lineA, lineB, lineC) => {
-  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) return true;
-  if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) return true;
-  if (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA)) return true;
-  return false;
-};
 
-const sCondition = (lineA, lineB, lineC) => {
-  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) return true;
-  return false;
-};
-
-const triangleCheck = (lA, lB, lC) => fCondition(lA, lB, lC) && sCondition(lA, lB, lC);
-
-console.log(triangleCheck(10, 14, 8));
+let arr = ['a', 'b', 'c'];
+console.log(arr.at[-1]);
